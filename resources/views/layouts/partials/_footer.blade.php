bfe4cd15d69b66830daf6a402d3b61dfe8299d1f
Ruairi McNicholas <rorymcnicholas@gmail.com>
2017-06-11 22:11:38 +0100

    Path update
    Path update

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,11 +1,11 @@
     <!-- jQuery -->
-    <script src="/dndlaravel/dndlaravel/public/assets/js/jquery.js"></script>
+    <script src="/assets/js/jquery.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="/dndlaravel/dndlaravel/public/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
 
     <!-- Morris Charts JavaScript -->
-    <script src="/dndlaravel/dndlaravel/public/assets/js/plugins/morris/raphael.min.js"></script>
-    <script src="/dndlaravel/dndlaravel/public/assets/js/plugins/morris/morris.min.js"></script>
-    <script src="/dndlaravel/dndlaravel/public/assets/js/plugins/morris/morris-data.js"></script>
+    <script src="/assets/js/plugins/morris/raphael.min.js"></script>
+    <script src="/assets/js/plugins/morris/morris.min.js"></script>
+    <script src="/assets/js/plugins/morris/morris-data.js"></script>
 
